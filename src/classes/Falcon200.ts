be07a83d9286a36e6b52587012adadac84be7336
CDjepeno <djepeno.christopher@gmail.com>
2021-08-15 23:34:00 +0200

    Refactoring

--- src/classes/Falcon200.ts
+++ src/classes/Falcon200.ts
@@ -2,12 +2,12 @@ import { BaseFly } from './BaseFly';
 export class Falcon200 extends BaseFly  {
   landing() {
     this.mediator.notify(this, 'landing Falcon200')
-    return `Attérissage du Falcon200`
+    return `Attérissage du Falcon 200`
   }
 
   takeOff() {
     this.mediator.notify(this, 'takeOff Falcon200')
-    return `Décollage du Falcon200`
+    return `Décollage du Falcon 200`
   }
 
 }
